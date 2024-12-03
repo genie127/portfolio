982d5a0695df8c86cfaeaff0d7a33ba038ad6a75
genie127 <twt770@naver.com>
2024-12-04 03:29:02 +0900

    router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
+
 import About from "./components/About";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Template from "./components/Template";
 
 function App() {
-  return (
-    <div className="App">
-      <Template>
-        <Header/>
-        <Main/>
-        <About/>
-      </Template>
-    </div>
+  return ( 
+        <div className="App">
+          <Template>
+            <Header/>
+            <Main/>
+            <About/>
+          </Template>
+      </div>
   );
 }
 
